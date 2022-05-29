955ecb881c0623449a95639154ea0dacc1ae9ffc
Popular Belbase <you@example.com>
2022-05-29 09:52:00 +0200

    nothing special changed

--- todo_rest/urls.py
+++ todo_rest/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+   path('accounts/', include('login_app.urls')),
    path('admin/', admin.site.urls),
    path('todo/', include('todo_app.urls')),
-   path('accounts/', include('login_app.urls')),
+   
 ]
